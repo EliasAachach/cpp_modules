4bfdce42c3b2fbe252218b91622b5269fdd46afe
Elias AACHACH <elaachac@e2r6p4.clusters.42paris.fr>
2022-01-26 17:16:52 +0100

    mkae fclean

--- cpp00/ex01/obj/Contact.d
+++
@@ -1,3 +0,0 @@
-obj/Contact.o: srcs/Contact.cpp srcs/../includes/Contact.hpp
-srcs/Contact.cpp:
-srcs/../includes/Contact.hpp:
--- cpp00/ex01/obj/PhoneBook.d
+++
@@ -1,5 +0,0 @@
-obj/PhoneBook.o: srcs/PhoneBook.cpp srcs/../includes/Phonebook.hpp \
-  includes/Contact.hpp
-srcs/PhoneBook.cpp:
-srcs/../includes/Phonebook.hpp:
-includes/Contact.hpp:
