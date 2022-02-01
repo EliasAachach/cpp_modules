fc1b3e6a7017ef2f326baf0f0ad6df39a0ee9cd8
Elias AACHACH <elaachac@e2r6p3.clusters.42paris.fr>
2022-02-01 16:53:21 +0100

    ex05 end

--- cpp01/ex05/obj/Karen.d
+++
@@ -1,3 +0,0 @@
-obj/Karen.o: srcs/Karen.cpp includes/Karen.hpp
-srcs/Karen.cpp:
-includes/Karen.hpp:
--- cpp01/ex05/obj/main.d
+++
@@ -1,3 +0,0 @@
-obj/main.o: srcs/main.cpp includes/Karen.hpp
-srcs/main.cpp:
-includes/Karen.hpp:
