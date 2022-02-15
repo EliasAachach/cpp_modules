c6bdf5652d6fed4bc5b97b2d34155b53abfa4e2d
Elias AACHACH <elaachac@e2r4p18.clusters.42paris.fr>
2022-02-15 12:10:08 +0100

    ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -7,19 +7,19 @@
 
 class NotFoundException : public std::exception
 {
-	virtual const char * what() cpnst throw()
+	virtual const char * what() const throw()
 	{
 		return ("Occurence not found in container.");
 	}
 };
 
 template <typename T>
-T	easyfind(T container, int toFind)
+int	easyfind(T container, int toFind)
 {
 	typename T::iterator iterator = std::find(container.begin(), container.end(), toFind);
 	if (iterator == container.end())
-		throw NotFoundException;
-	return(iterator);
+		throw NotFoundException();
+	return(*iterator);
 }
 
 #endif
